fc4917a9ab8b4629ae19fdb59a97366afe0d541e
manish <m.wire2511@gmail.com>
2024-10-22 22:18:52 +0530

    first commit

---
+++ calculator/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div id='main'>
+      <h1>
+        Calculator
+      </h1>
+    </div>
+  )
+}
+
+export default App
